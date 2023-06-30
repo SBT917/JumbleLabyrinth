141d72d6136e75fcf7738b27f332b54572726ce4
ci <ci@jec.ac.jp>
2023-06-30 12:32:59 +0900

    crying

--- Assets/Script/MazeRenderer.cs
+++ Assets/Script/MazeRenderer.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class MazeRenderer : MonoBehaviour
 {
-
     [SerializeField]
     [Range(1, 50)]
     private int width = 10;
-
-    [SerializeField]
-    [Range(1, 50)]
-    private int height = 10;
 
     [SerializeField]
     private float size = 1f;
@@ -25,62 +20,52 @@ public class MazeRenderer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var maze = MazeGenerator.Generate(width, height);
+        var maze = MazeGenerator.Generate(width, width); // Generate a square maze
         Draw(maze);
     }
 
     private void Draw(WallState[,] maze)
     {
-
         var floor = Instantiate(floorPrefab, transform);
-        floor.localScale = new Vector3(width, 1, height);
+        floor.localScale = new Vector3(width, 1, width);
 
         for (int i = 0; i < width; ++i)
         {
-            for (int j = 0; j < height; ++j)
+            for (int j = 0; j < width; ++j)
             {
                 var cell = maze[i, j];
-                var position = new Vector2(-width / 2 + i, -height / 2 + j);
+                var position = new Vector3(i * size, 0, j * size);
 
                 if (cell.HasFlag(WallState.UP))
                 {
                     var topWall = Instantiate(wallPrefab, transform) as Transform;
-                    topWall.position = position + new Vector2(0, size / 2);
+                    topWall.position = position + new Vector3(size / 2, 0, size);
                     topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
                 }
 
                 if (cell.HasFlag(WallState.LEFT))
                 {
                     var leftWall = Instantiate(wallPrefab, transform) as Transform;
-                    leftWall.position = position + new Vector2(-size / 2, 0);
-                    leftWall.localScale = new Vector2(size, leftWall.localScale.y);
-                    leftWall.eulerAngles = new Vector2(0,90);
+                    leftWall.position = position + new Vector3(0, 0, size / 2);
+                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.eulerAngles = new Vector3(0, 90, 0);
                 }
 
-                if (i == width - 1)
+                if (cell.HasFlag(WallState.RIGHT))
                 {
-                    if (cell.HasFlag(WallState.RIGHT))
-                    {
-                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
-                        rightWall.position = position + new Vector2(+size / 2, 0);
-                        rightWall.localScale = new Vector2(size, rightWall.localScale.y);
-                        rightWall.eulerAngles = new Vector3(0,90);
-                    }
+                    var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                    rightWall.position = position + new Vector3(size, 0, size / 2);
+                    rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                    rightWall.eulerAngles = new Vector3(0, 90, 0);
                 }
 
-                if (j == 0)
+                if (cell.HasFlag(WallState.DOWN))
                 {
-                    if (cell.HasFlag(WallState.DOWN))
-                    {
-                        var bottomWall = Instantiate(wallPrefab, transform) as Transform;
-                        bottomWall.position = position + new Vector2(0, -size / 2);
-                        bottomWall.localScale = new Vector2(size, bottomWall.localScale.y);
-                    }
+                    var bottomWall = Instantiate(wallPrefab, transform) as Transform;
+                    bottomWall.position = position + new Vector3(size / 2, 0, 0);
+                    bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
                 }
             }
-
         }
-
     }
-
 }
