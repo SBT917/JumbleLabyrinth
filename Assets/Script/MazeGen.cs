1af119d6c14a41c85b39b8327f3ca4d221b232c9
ci <ci@jec.ac.jp>
2023-07-07 12:26:28 +0900

    code restructured

--- Assets/Script/MazeGen.cs
+++ Assets/Script/MazeGen.cs
@@ -8,22 +8,30 @@ public class MazeGen : MonoBehaviour
 {
     //ƒ^ƒCƒ‹ƒ}ƒbƒv“Ç‚İ‚İ—p
     [SerializeField]
-    private Tilemap tilemap1;
+    private Tilemap maze;
     [SerializeField]
-    private TileBase tiledata0, tiledata9;
+    private TileBase floorTile, wallTile, finishTile;
 
+    
     //–À˜H‚ÌÅ‘å”
     [SerializeField]
     private int i_max_x, i_max_y;
 
     //ì¬‚µ‚½–À˜H‚ÌŠi”[•Ï”
-    int[,] i_map;
+    private int[,] i_map;
 
-    // Start is called before the first frame update
+    private const int Floor = 0, Wall = 9, Finish = -1;
+
+
+    private Vector2 finishLocation;
     void Start()
     {
-
-
+        finishLocation = new Vector2Int(Random.Range(3, i_max_x), Random.Range(3, i_max_y));
+        Debug.Log(finishLocation);
+        GenerateMaze();
+    }
+    private void GenerateMaze()
+    {
         //–_“|‚µ•û‚Å–À˜Hì¬
 
         int i_temp;
@@ -31,17 +39,7 @@ public class MazeGen : MonoBehaviour
         i_map = new int[i_max_x + 1, i_max_y + 1];
 
         //ŠO•Ç‚ğ•Ç‚Å–„‚ß‚é
-        for (int i = 1; i <= i_max_x; i++)
-        {
-            i_map[i, 1] = 9;
-            i_map[i, i_max_y] = 9;
-        }
-
-        for (int i = 1; i <= i_max_y; i++)
-        {
-            i_map[1, i] = 9;
-            i_map[i_max_x, i] = 9;
-        }
+        FillOuterWalls();
 
         //–_“|‚µ•û‚Å–À˜H‚ğì¬(­‚µè”²‚«)
         i_temp = 4;
@@ -56,43 +54,66 @@ public class MazeGen : MonoBehaviour
             for (int x = 3; x <= i_max_x - 1; x += 2)
             {
                 i_map[x, y] = 9;
-                switch (Random.Range(0, i_temp))
+                switch (Random.Range(Floor, i_temp))
                 {
                     case 0:
-                        i_map[x + 1, y] = 9;
+                        i_map[x + 1, y] = Wall;
                         break;
                     case 1:
-                        i_map[x - 1, y] = 9;
+                        i_map[x - 1, y] = Wall;
                         break;
                     case 2:
-                        i_map[x, y + 1] = 9;
+                        i_map[x, y + 1] = Wall;
                         break;
                     case 3:
                         if (y == 3)
                         {
-                            i_map[x, y - 1] = 9;
+                            i_map[x, y - 1] = Wall;
                         }
                         break;
                 }
             }
-
+            FillMazeTiles();
         }
+    }
+    private void FillOuterWalls()
+    {
+        for (int i = 1; i <= i_max_x; i++)
+        {
+            i_map[i, 1] = Wall;
+            i_map[i, i_max_y] = Wall;
+        }
+
+        for (int i = 1; i <= i_max_y; i++)
+        {
+            i_map[1, i] = Wall;
+            i_map[i_max_x, i] = Wall;
+        }
+    }
+    private void FillMazeTiles()
+    {
 
         //ƒ}ƒbƒv‚É”½‰f –À˜H‚Ì^‚ñ’†‚ğƒXƒ^[ƒg‚É‚µ‚½‚¢‚Ì‚ÅA”½‰fêŠ‚ğ”¼•ª‚¸‚ç‚µ‚Ä‚¢‚é 0’Ê‰ß 9•Ç
         for (int x = 1; x <= i_max_x; x++)
         {
             for (int y = 1; y <= i_max_y; y++)
             {
-                if (i_map[x, y] == 0)
+                if (i_map[x, y] == Floor)
                 {
-                    tilemap1.SetTile(new Vector3Int(x - (i_max_x / 2), y - (i_max_y / 2), 0), tiledata0);
+                    if (new Vector2Int(x, y) == finishLocation)
+                    {
+                        maze.SetTile(new Vector3Int(x - (i_max_x / 2), y - (i_max_y / 2), 0), finishTile);
+                    }
+                    else
+                    {
+                        maze.SetTile(new Vector3Int(x - (i_max_x / 2), y - (i_max_y / 2), 0), floorTile);
+                    }
                 }
-                else
+                else if (i_map[x,y]==Wall)
                 {
-                    tilemap1.SetTile(new Vector3Int(x - (i_max_x / 2), y - (i_max_y / 2), 0), tiledata9);
+                    maze.SetTile(new Vector3Int(x - (i_max_x / 2), y - (i_max_y / 2), 0), wallTile);
                 }
             }
         }
     }
-
 }
