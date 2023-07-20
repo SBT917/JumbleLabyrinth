505ef8123d694388f47bdfd6a07bef6a2903dfae
Iino_CI04 <22ci0403@jec.ac.jp>
2023-07-20 15:13:48 +0900

    ※作成途中のため、このコミットの敵は正常に動作しない プレファブの設定ミスを修正中　新たな敵の素体を追加

---
+++ Assets/Iino/Script/ProjectileExplosion.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //TODO:プレイヤーをスタンさせる
+            //コライダーをオフに
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+    public void DestroyExplosion()
+    {
+        Destroy(gameObject);
+    }
+}
